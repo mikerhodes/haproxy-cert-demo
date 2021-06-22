2afe55d33c517b42be6e20cf0ce63e3ede110685
Mike Rhodes <mike.rhodes@gmail.com>
2021-06-22 10:33:19 +0100

    Use production-ready Go code

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 // A simple client / server to demonstrate using a CA to use mutual certificate
-// authentication using HAProxy.
+// authentication using HAProxy. The client and server include reasonable
+// default configurations for production use, because several Go defaults
+// don't suit production servers.
 package main
 
 import (
@@ -34,10 +36,23 @@ func runClient() {
 	url := fmt.Sprintf("http://localhost:%d", clientPort)
 	log.Printf("Starting client: GET %s.", url)
 
+	// This client (somewhat needlessly) uses a reasonable set of defaults for
+	// API production use in terms of the c's Timeout and the Transport's
+	// connection pool.
+	t := http.DefaultTransport.(*http.Transport).Clone()
+	t.MaxIdleConns = 100
+	t.MaxConnsPerHost = 10
+	t.MaxIdleConnsPerHost = 10
+
+	c := &http.Client{
+		Timeout:   1 * time.Second,
+		Transport: t,
+	}
+
 	for {
 		time.Sleep(1 * time.Second)
 
-		resp, err := http.Get(url)
+		resp, err := c.Get(url)
 		if err != nil {
 			log.Printf("Error accessing %s: %v", url, err)
 			continue
@@ -55,12 +70,27 @@ func runClient() {
 func runServer() {
 	log.Printf("Starting server on port %d.", serverPort)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Print("Received request.")
-		fmt.Fprintln(w, "Hello")
-	})
+	// This shows a reasonable default Go server configuration to expose
+	// directly to the internet. Primarily it includes timeouts to prevent
+	// evil clients hogging all the connections. Likely TLS should be included
+	// but isn't here because the point of the example is to show the HAProxy
+	// secure connection.
+	mux := http.NewServeMux()
+	mux.Handle("/", apiHandler{})
 
-	log.Fatal(http.ListenAndServe(
-		fmt.Sprintf("localhost:%d", serverPort),
-		nil))
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("localhost:%d", serverPort),
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	log.Println(srv.ListenAndServe())
+}
+
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	log.Print("Received request.")
+	fmt.Fprintln(w, "Hello")
 }
